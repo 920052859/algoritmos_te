694a36fd742d831d7e70231f6173fe2a38f14f4c
Corina Lazo <60791687+920052859@users.noreply.github.com>
2021-09-26 15:36:48 -0500

    Erro de sintaxe incorreta
    Erro de sintaxe incorreta ocorre na linha 12

--- unidade_2/6.py
+++ unidade_2/6.py
@@ -9,4 +9,4 @@ O programa le o raio de um circulo e imprime a area na tela'''
 
 raio = float(input("Informe o raio do círculo: "))
 area = 3.14 * raio * raio
-print(Área: ', area)
+print('Área: ', area)
